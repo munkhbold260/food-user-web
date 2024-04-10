2c9af540cfa7ac2d1b4f528f97d7e469076f6c8a
bold_max <munkhbold260@gmail.com>
2024-04-11 00:05:59 +0800

    mail send

--- src/components/login/StepOne.tsx
+++ src/components/login/StepOne.tsx
@@ -8,14 +8,35 @@ import {
 } from "@mui/material";
 
 export const StepOne = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const form = new FormData(event.currentTarget);
-    const data = {
-      email: form.get("email"),
-    };
-    console.log("data", data);
-  };
+    const data = { email: form.get("email") };
+    try {
+      const response = await fetch("http://localhost:4000/api/reset-pass", {
+        method: "POST",
+        body: JSON.stringify(data),
+        mode: "cors",
+
+        headers: {
+          Accept: "application.json",
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        console.log("falling over");
+        throw new Error(`response status: ${response.status}`);
+      }
+      const responseData = await response.json();
+      console.log(responseData["message"]);
+
+      alert("Message successfully sent");
+    } catch (err) {
+      console.error(err);
+      alert("Error, please try resubmitting the form");
+    }
+  }
 
   return (
     <Container>
